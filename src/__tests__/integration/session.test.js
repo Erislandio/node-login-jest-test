d1cb78eecb5b89dd743d60a528273e6e31748909
Erislândio Soares <erislandio.soares@acct.global>
2019-07-11 00:15:12 -0300

    add teste runner

---
+++ src/__tests__/integration/session.test.js
@@ -0,0 +1,10 @@
+describe("Authentication", () => {
+  it("should receive JWT token when authenticate with credentials", () => {
+    const a = 3;
+    const b = 4;
+
+    const sum = a + b;
+
+    expect(sum).toBe(7);
+  });
+});

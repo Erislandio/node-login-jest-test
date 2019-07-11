4fd0e158bc5982fe5155cae938337d26f018294d
Erislândio Soares <erislandio.soares@acct.global>
2019-07-10 23:55:51 -0300

    teste static name, email and pass - add color terminal dependncie

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 const app = require("./app");
+const chalk = require('chalk')
+const PORT = process.env.PORT || 3000
 
-app.listen(process.env.PORT || 3000);
+app.listen(PORT, () => {
+    console.log(chalk.green(`Server executando na porta ${PORT}`))
+});
 
 

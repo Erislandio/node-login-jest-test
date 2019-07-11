0641e002aafdc01a4c5818a19f318b3ec8716e04
Erislândio Soares <erislandio.soares@acct.global>
2019-07-10 23:59:51 -0300

    add script start

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,4 @@
 const routes = require("express").Router();
 const { User } = require("./app/models");
 
-User.create({
-  name: "Maria Cleuda soares",
-  email: "maria.soares@gmail.com",
-  password_hash: "mariaMia"
-});
-
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const chalk = require('chalk')
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(chalk.green(`Server executando na porta ${PORT}`))
+    console.log(chalk.blue(`Server executando na porta ${PORT}`))
 });
 
 

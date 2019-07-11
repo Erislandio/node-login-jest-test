f9f75c3d0d4f4cdcd358211193c98c95f51d2696
Erislândio Soares <erislandio.soares@acct.global>
2019-07-10 23:15:38 -0300

    add dependendies and routes

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,3 @@
-const express = require("express");
+const routes = require("express").Router();
 
-class AppController {
-  constructor() {
-    this.express = express();
-    this.middlewares();
-    this.routes();
-  }
-
-  middlewares() {
-    this.express.use(express.json());
-  }
-
-  routes() {
-    this.express.use(require("./routes"));
-  }
-}
-
-module.exports = new AppController().express;
+module.exports = routes;
